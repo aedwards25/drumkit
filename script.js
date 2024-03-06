9151b395d4251d9b81325d8abbb8876fe09eca87
Angelique Edwards <angie@Angies-MacBook-Pro.local>
2024-03-06 15:12:48 -0500

    Final changes

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 function removeTransition(e) {
-    if (e.PropertyName !== transform) return;
+    if (e.propertyName !== "transform") return;
 
-    e.target.classList.remove("playing");
+    e.target.classList.remove('playing');
 }
 
-function audio(e) {
-let sound = document.querySelector (`audio[data-key="${e.keyCode}"]`)
-let key = document.querySelector(`div[data-key="${e.keyCode}"]`)
+function playSounds(e) {
+var sound = document.querySelector (`audio[data-key="${e.keyCode}"]`);
+var key = document.querySelector(`div[data-key="${e.keyCode}"]`);
 
 if (!sound) return;
-key.classList.add("playing");
+
+key.classList.add('playing');
 
 sound.currentTime = 0;
 sound.play();
 }
 
+var keys = Array.from(document.querySelectorAll('.key'));
 
-const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(function(key) {
+    key.addEventListener('transitionend', removeTransition);
+});
+
+window.addEventListener('keydown', playSounds);
